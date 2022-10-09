7b00ffce65389f2d2064f5afd0ac19a411ff71e9
Anand Kumar Jha <66514444+anandjha-git@users.noreply.github.com>
2022-10-09 11:34:24 +0530

    Create Application -> .jar file -> Dockerfile

---
+++ ZuulExample/src/main/java/com/download/example/ZuulExampleApplication.java
@@ -0,0 +1,13 @@
+package com.download.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZuulExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZuulExampleApplication.class, args);
+	}
+
+}
